f5aa278faed621977b582bf0eaffed4495e26f1c
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-04-11 02:44:46 -0300

    chore: update the `app.models` imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,7 @@
-from app.models.user import (  # noqa: F401
-    User,
-    Workspace,
-    WorkspaceUser,
-)
+from app.models.base import Base  # noqa: F401
+from app.models.org import Job  # noqa: F401
+
+__all__ = [
+    "Base",
+    "Job",
+]
