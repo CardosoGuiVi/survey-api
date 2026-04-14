0ac96c788f9c4ab03d76265cec738432662f42b8
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-04-14 16:40:52 -0300

    feat: add `Position` models

--- app/models/org/__init__.py
+++ app/models/org/__init__.py
@@ -1,4 +1,5 @@
 from .department import Department  # noqa: F401
 from .job import Job  # noqa: F401
+from .position import Position  # noqa: F401
 
-__all__ = ["Job", "Department"]
+__all__ = ["Department", "Job", "Position"]
