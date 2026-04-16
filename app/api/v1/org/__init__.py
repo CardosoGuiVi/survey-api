19f5e5531dcab4fe4f85a024507ef976488dc52d
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-04-16 00:28:41 -0300

    feat: add department org

--- app/api/v1/org/__init__.py
+++ app/api/v1/org/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from .department import router as department_router
 from .job import router as job_router
 
 router = APIRouter()
 
 router.include_router(job_router, prefix="/org", tags=["Org - Jobs"])
+router.include_router(department_router, prefix="/org", tags=["Org - Department"])
