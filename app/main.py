c4c5a256319a055623ce53cfe9b0b7b4236e17e5
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-04-16 12:08:07 -0300

    refactor: remove auth for MVP

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.v1.auth import router as auth_router
 from app.api.v1.org import router as org_router
 from app.core.config import settings
 
@@ -19,7 +18,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth_router, prefix="/api/v1")
 app.include_router(org_router, prefix="/api/v1")
 
 
