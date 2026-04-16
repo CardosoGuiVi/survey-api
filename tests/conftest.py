673b0e4b0760e4dc15340dec39646db7bdd8a226
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-04-16 00:30:52 -0300

    feat: add `client` fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
+from collections.abc import AsyncGenerator
+
 import pytest
+import pytest_asyncio
 from alembic import command
 from alembic.config import Config
+from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     create_async_engine,
 )
 
 from app.core.config import settings
+from app.core.database import get_session
+from app.main import app
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -41,3 +47,17 @@ async def db_session(engine):
 
         await session.close()
         await transaction.rollback()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def client(db_session: AsyncSession) -> AsyncGenerator[AsyncClient, None]:
+    async def override_get_session() -> AsyncGenerator[AsyncSession, None]:
+        yield db_session
+
+    app.dependency_overrides[get_session] = override_get_session
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+    ) as ac:
+        yield ac
+    app.dependency_overrides.clear()
