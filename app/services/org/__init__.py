b9325cd785507af967a6c8fd07bf460ba32e83be
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-04-16 00:23:19 -0300

    feat: add `job_service`

--- app/services/org/__init__.py
+++ app/services/org/__init__.py
@@ -1,3 +1,10 @@
+from .department_service import (  # noqa: F401
+    create_department,
+    delete_department,
+    get_department,
+    list_departments,
+    update_department,
+)
 from .job_service import (  # noqa: F401
     create_job,
     delete_job,
@@ -7,6 +14,11 @@ from .job_service import (  # noqa: F401
 )
 
 __all__ = [
+    "create_department",
+    "delete_department",
+    "get_department",
+    "list_departments",
+    "update_department",
     "create_job",
     "delete_job",
     "get_job",
--- app/services/org/job_service.py
+++ app/services/org/job_service.py
@@ -7,10 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.org import (
     Job,
 )
-from app.schemas.org import (
-    JobCreate,
-    JobUpdate,
-)
+from app.schemas.org.job import JobCreate, JobUpdate
 
 
 async def create_job(session: AsyncSession, payload: JobCreate) -> Job:
