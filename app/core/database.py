adca46be3b66035a1465c09dd15ff983103a61c4
Guilherme Cardoso <cardoso.guivi@gmail.com>
2026-04-30 12:10:19 -0300

    fix: remove `AsyncDB` type.

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,5 @@
 from collections.abc import AsyncGenerator
-from typing import Annotated
 
-from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
@@ -27,6 +25,3 @@ class Base(DeclarativeBase):
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
-
-
-AsyncDB = Annotated[AsyncSession, Depends(get_session)]
